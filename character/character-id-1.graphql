a4129ce85420944873a85202d4c02c0855c9dae2
Eyuel M. Gedlie <eyuel.mazengia@aastustudent.edu.et>
2025-08-13 15:05:41 +0300

    final commit

---
+++ character/character-id-1.graphql
@@ -0,0 +1,34 @@
+query GetCharacters {
+  character1: character(id: 1) {
+    id
+    name
+    status
+    species
+    type
+    gender
+  }
+  character2: character(id: 2) {
+    id
+    name
+    status
+    species
+    type
+    gender
+  }
+  character3: character(id: 3) {
+    id
+    name
+    status
+    species
+    type
+    gender
+  }
+  character4: character(id: 4) {
+    id
+    name
+    status
+    species
+    type
+    gender
+  }
+}
